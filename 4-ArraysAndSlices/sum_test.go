81357e870c063b2a82d9d4cbf2c5d2a8de605f41
paulohlips <paulohfcmendes@gmail.com>
2023-10-14 15:39:10 -0300

    feat: refac test and sum to use slices

--- 4-ArraysAndSlices/sum_test.go
+++ 4-ArraysAndSlices/sum_test.go
@@ -3,11 +3,17 @@ package arraysandslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("Should return sum equals 15", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	expected := 15
+		got := Sum(numbers)
+		expected := 15
 
+		sumAssertionChecker(t, got, expected, numbers)
+	})
+}
+
+func sumAssertionChecker(t *testing.T, got int, expected int, numbers []int) {
 	if got != expected {
 		t.Errorf("got %d, but expected %d given %v", got, expected, numbers)
 	}
