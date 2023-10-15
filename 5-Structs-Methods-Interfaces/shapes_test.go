f0967866d373f30b2ffce910f94258d36539d1f9
paulohlips <paulohfcmendes@gmail.com>
2023-10-15 11:54:37 -0300

    feat: using interface and helper function

--- 5-Structs-Methods-Interfaces/shapes_test.go
+++ 5-Structs-Methods-Interfaces/shapes_test.go
@@ -13,23 +13,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, expected float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != expected {
+			t.Errorf("got %.g but expected %.g", got, expected)
+		}
+	}
 	t.Run("should calc rectangle area", func (t *testing.T)  {
 		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		expected := 100.0
-
-		if got != expected {
-			t.Errorf("got %.2f but expected %.2f", got, expected)
-		}
+		checkArea(t, rectangle, 100.0)
 	})
 
 	t.Run("should calc circle area", func(t *testing.T) {
 		circle :=  Circle{10.0}
-		got := circle.Area()
-		expected := 314.1592653589793
+		checkArea(t, circle, 314.1592653589793)
 
-		if got != expected {
-			t.Errorf("got %.g but expected %.g", got, expected)
-		}
 	})
 }
