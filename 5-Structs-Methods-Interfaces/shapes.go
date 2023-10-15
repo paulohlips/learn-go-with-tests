8ac7fafc26b1b058ed5cb46e2a9f71afdf5eac0a
paulohlips <paulohfcmendes@gmail.com>
2023-10-15 11:28:47 -0300

    feat: using methods to calc area

--- 5-Structs-Methods-Interfaces/shapes.go
+++ 5-Structs-Methods-Interfaces/shapes.go
@@ -1,15 +1,28 @@
 package structsmethodsinterfaces
 
+import "math"
+
 type Rectangle struct {
-	width float64
-	height float64
+	Width float64
+	Height float64
 }
 
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
+}
 
 func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.width + rectangle.height)
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+	return rectangle.Width * rectangle.Height
 }
--- 5-Structs-Methods-Interfaces/shapes_test.go
+++ 5-Structs-Methods-Interfaces/shapes_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	expected := 100.0
+	t.Run("should calc rectangle area", func (t *testing.T)  {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
+		expected := 100.0
 
-	if got != expected {
-		t.Errorf("got %.2f but expected %.2f", got, expected)
-	}
+		if got != expected {
+			t.Errorf("got %.2f but expected %.2f", got, expected)
+		}
+	})
+
+	t.Run("should calc circle area", func(t *testing.T) {
+		circle :=  Circle{10.0}
+		got := circle.Area()
+		expected := 314.1592653589793
+
+		if got != expected {
+			t.Errorf("got %.g but expected %.g", got, expected)
+		}
+	})
 }
