a9745ddf831e56303ecf9841b1a459b6d2491077
paulohlips <paulohfcmendes@gmail.com>
2023-10-15 11:11:14 -0300

    feat: perimeter and area

---
+++ 5-Structs-Methods-Interfaces/shapes_test.go
@@ -0,0 +1,21 @@
+package structsmethodsinterfaces
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	expected := 40.0
+
+	if got != expected {
+		t.Errorf("got %.2f but expected %.2f", got, expected)
+	}
+}
+
+func TestArea(t *testing.T) {
+	got := Area(10.0, 10.0)
+	expected := 100.0
+
+	if got != expected {
+		t.Errorf("got %.2f but expected %.2f", got, expected)
+	}
+}
