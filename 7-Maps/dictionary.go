2ad6eef2f22aeb18d346df57c6c94027ee4d40d0
paulohlips <paulohfcmendes@gmail.com>
2023-10-19 08:58:32 -0300

    feat: adds new word method

--- 7-Maps/dictionary.go
+++ 7-Maps/dictionary.go
@@ -1,7 +1,33 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+var ErrNotFound = errors.New("could not find the word you ware looking for")
+var ErrWordExists = errors.New("cannot ad word because is already exists")
+
+func (d Dictionary) Search(word string) (string, error) {
+  definition, ok :=	d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
